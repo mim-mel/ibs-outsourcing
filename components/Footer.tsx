d09b0d72662af6089c904172ff1503933877c288
mim-mel <mfblade2014@gmail.com>
2023-09-05 17:44:07 +0900

    design: Footer 반응형 적용

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,22 +13,20 @@ const Footer = () => {
       <ContentWrap>
         <Logo src='/image/footer-logo.png' />
         <TextWrap>
-          <TextBox>
+          <IpadLogo src='/image/footer-logo.png' />
+          <TextBox grid>
             <Text>
-              <span>ADDRESS</span>서울 서초구 동작대로 214 6층
-            </Text>
-            <Text>|</Text>
-            <Text>
-              <span>TEL</span>02-6951-3323
-            </Text>
-            <Text>|</Text>
-            <Text>
-              <span>FAX</span>02-6951-7002
+              <TextBold isMarginLeftNone isIpadMarginLeftNone>
+                ADDRESS
+              </TextBold>
+              서울 서초구 동작대로 214 6층
+              <TextBold> TEL</TextBold>02-6951-3323
+              <TextBold> FAX</TextBold>02-6951-7002
+              <TextBold isMarginLeftNone> E-MAIL</TextBold>
+              ibspartners.seoul@gmail.com
             </Text>
           </TextBox>
-          <Text>
-            <span>E-MAIL</span>ibspartners.seoul@gmail.com
-          </Text>
+
           <Copyright>
             Copyright IBS & AH Corparation All rights reserved.
           </Copyright>
@@ -44,6 +42,9 @@ const Footer = () => {
 
 interface Props {
   isMarginRightNone?: boolean;
+  isMarginLeftNone?: boolean;
+  isIpadMarginLeftNone?: boolean;
+  grid?: boolean;
 }
 
 const Wrap = styled.div`
@@ -62,12 +63,21 @@ const TitleBox = styled.div`
   justify-content: space-between;
   margin: 0 auto;
   width: 600px;
+
+  @media screen and (max-width: 760px) {
+    width: 350px;
+  }
 `;
 
 const Title = styled.div<Props>`
   color: #fff;
   font-size: 16px;
   font-weight: 300;
+  cursor: pointer;
+
+  @media screen and (max-width: 760px) {
+    font-size: 15px;
+  }
 `;
 
 const ContentWrap = styled.div`
@@ -76,41 +86,113 @@ const ContentWrap = styled.div`
   display: grid;
   grid-template-columns: 18% 40% 20%;
   justify-content: space-between;
+
+  @media screen and (max-width: 1024px) {
+    width: 700px;
+    grid-template-columns: 60% 30%;
+    margin: 85px auto;
+  }
+
+  @media screen and (max-width: 760px) {
+    width: 350px;
+    grid-template-columns: 100%;
+  }
 `;
 
 const Logo = styled.img`
   margin-bottom: 13px;
+
+  @media screen and (max-width: 1024px) {
+    display: none;
+  }
 `;
 
-const TextWrap = styled.div``;
+const IpadLogo = styled.img`
+  display: none;
 
-const TextBox = styled.div`
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    position: absolute;
+    top: -52px;
+  }
+
+  @media screen and (max-width: 760px) {
+    width: 177px;
+    top: -40px;
+  }
+`;
+
+const TextWrap = styled.div`
+  position: relative;
+`;
+
+const TextBox = styled.div<Props>`
   width: 600px;
   display: flex;
+  line-height: 26px;
   justify-content: space-between;
-  margin-bottom: 10px;
+
+  @media screen and (max-width: 1024px) {
+    width: 508px;
+  }
+
+  @media screen and (max-width: 760px) {
+    width: 320px;
+    margin-top: 20px;
+  }
 `;
 
 const Text = styled.div`
   color: #d4d4d4;
   font-size: 15px;
   font-weight: 300;
+  position: relative;
 
-  span {
-    margin-right: 12px;
-    font-weight: 500;
+  @media screen and (max-width: 760px) {
+    font-size: 14px;
   }
+`;
+
+const TextBold = styled.span<Props>`
+  margin-left: ${props => (props.isMarginLeftNone ? '' : '20px')};
+  margin-right: 12px;
+  font-weight: 500;
+
+  @media screen and (max-width: 1024px) {
+    margin-left: ${props => (props.isIpadMarginLeftNone ? '' : '20px')};
+  }
+`;
+
+const Line = styled.div`
+  height: 15px;
+  width: 0.1px;
+  background-color: #d4d4d4;
+  position: absolute;
+  left: -12px;
+  top: 6px;
+  opacity: 0.4;
 `;
 
 const Copyright = styled.div`
   margin-top: 22px;
-  color: #525252;
-  font-size: 15px;
+  color: #636363;
+  font-size: 14px;
   font-weight: 300;
+
+  @media screen and (max-width: 760px) {
+    font-size: 13px;
+  }
 `;
 
 const NumberWrap = styled.div`
   margin-left: auto;
+
+  @media screen and (max-width: 760px) {
+    margin-right: auto;
+    margin-left: 0;
+    position: relative;
+    top: 35px;
+  }
 `;
 
 const NumberText = styled.div`
@@ -118,12 +200,20 @@ const NumberText = styled.div`
   font-size: 18px;
   font-weight: 300;
   margin-bottom: 5px;
+
+  @media screen and (max-width: 760px) {
+    font-size: 15px;
+  }
 `;
 
 const Number = styled.div`
   color: #fff;
   font-size: 30px;
   font-weight: 700;
+
+  @media screen and (max-width: 760px) {
+    font-size: 24px;
+  }
 `;
 
 export default Footer;
